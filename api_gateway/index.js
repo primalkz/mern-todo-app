995dd46f2d79396f26442fe22e75a895a3f22aea
primalkz <44187697+primalkz@users.noreply.github.com>
2024-04-25 13:21:46 +0530

    require cors

--- api_gateway/index.js
+++ api_gateway/index.js
@@ -4,9 +4,11 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 dotenv.config();
 
